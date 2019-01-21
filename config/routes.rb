31084a31d41d6b72846ea4fdb2953c105099aa64
taku <takuma20181120@gmail.com>
2019-01-21 10:48:26 +0000

    user registration bug-fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
   
-  get 'signup', to: 'user#new'
-  resources :user, only: [:show, :new, :create]
+  get 'signup', to: 'users#new'
+  resources :users, only: [:show, :new, :create]
 end
